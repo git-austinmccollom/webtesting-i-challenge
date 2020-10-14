20f7621c8f243077fcfcf0cfad15b0f382baedfa
git-austinmccollom <austinmccollom@gmail.com>
2020-10-14 12:02:08 -0500

    added jest and tested repair

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,16 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+
+
+test('repair() should take an object and return an object with durability 100', () => {
+    const item = {
+        name: "mithril armor",
+        durability: 50,
+        enhancement: 0
+    }
+
+    const newItem = enhancer.repair(item)
+    
+    expect(newItem.durability).toBe(100);
+});
